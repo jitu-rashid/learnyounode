332a383665fd6d202ab96c183a8efea9e6d30cfe
jitu <jitu757@gmail.com>
2015-08-19 00:29:13 -0400

    Created BABY STEPS using learnyounode

--- program_baby_steps.js
+++ program_baby_steps.js
@@ -1,8 +1,8 @@
-var i, arguments, sum=0;
+var i, arguments, sum = 0;
 
 arguments = process.argv;
 
-for (i=2; i < arguments.length; i++) {
+for (i = 2; i < arguments.length; i++) {
 	sum = sum + Number(arguments[i]);
 };
 
